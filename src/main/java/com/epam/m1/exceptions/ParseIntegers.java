3e613800fc8e223308b56b04852ee7e5b9822d9d
Нодир Асомов <nasomov93@gmail.com>
2023-11-02 19:17:01 +0500

    hi

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -1,5 +1,6 @@
 package com.epam.m1.exceptions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -10,22 +11,36 @@ import java.util.List;
  */
 public class ParseIntegers {
 
-    private static final List<String> WORDS =
+    static List<String> WORDS =
+
             Arrays.asList(
                     "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
                             .split(" "));
 
+
     public static void main(String[] args) {
+
         Iterator<String> words = WORDS.iterator();
+
         int sum = 0;
-        String justWords = "";
+        String justWords = " ";
         while (words.hasNext()) {
             String next = words.next();
             int number = Integer.parseInt(next);
-            // todo: complete it
+            try {
+                    justWords+=next;
+            }catch (NumberFormatException e){
+                sum+=number;
+
+            }
+            System.out.println(justWords);
+            System.out.println(sum);
+
+
         }
-        System.out.println("Sum is " + sum);
-        System.out.println("Just words:" + justWords);
+
+        //System.out.println("Sum is " + sum);
+        //System.out.println("Just words:" + justWords);
     }
 }
 
